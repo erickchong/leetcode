3d05867da7a20c6fcafcf8008332fa13914f4c68
Erick Chong <erickychong@icloud.com>
2018-12-14 22:15:47 -0800

    Update #61 solution

--- 61. Rotate List/solution.cpp
+++ 61. Rotate List/solution.cpp
@@ -9,17 +9,19 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if (!head) return head;
+        if (head == nullptr) return head;
         ListNode* newHead = getNewHead(head, k);
         ListNode* result = new ListNode(newHead->val);
         ListNode* newHeadCopy = newHead;
         ListNode* resultCopy = result;
         
+        /* Copy from new head until end of original list*/
         while (newHeadCopy->next != nullptr) {
             resultCopy->next = new ListNode(newHeadCopy->next->val);
             resultCopy = resultCopy->next;
             newHeadCopy = newHeadCopy->next;
         }
+        /* Copy from original head until new head */
         while (head != newHead) {
             resultCopy->next = new ListNode(head->val);
             resultCopy = resultCopy->next;
@@ -32,22 +34,25 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         int listSize = 0;
-        /* Calculate list size first */
+
         while (fast != nullptr) {
             fast = fast->next;
             listSize++;
         }
        
-        int trueK = (k >= listSize) ? k % listSize : k;
+        /* k can get very large, so figure out "true" number of rotations */
+        int trueK = k % listSize;
             
-        if (trueK == 0) return head;
+        if (trueK == 0) return head; // No need for rotation
         
+        /* Calculate how much further fast pointer will be */
         fast = head;
         while (trueK > 0) {
             fast = fast->next;
             trueK--;
         }
         
+        /* Calculate new head using the help of the fast pointer */
         while (fast != nullptr) {
             fast = fast->next;
             slow = slow->next;
